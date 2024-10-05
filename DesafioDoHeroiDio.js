20ffb7ebc03b5e17d47fe1ff9e0b80171dad204f
WespertoW <ghreinocraft@gmail.com>
2024-10-05 14:40:51 -0300

    Desafio Do Heroi Dio

---
+++ DesafioDoHeroiDio.js
@@ -0,0 +1,26 @@
+let XP = 1000
+let NomeHeroi = "bengala torta"
+var Elo = XP
+
+
+
+//Validação da quantidade de XP
+if (XP <= 1000) {
+    ELO = "ferro"
+}else if (XP >= 1001 && XP <=2000 ) {
+    ELO = "Bronze"
+}else if (XP >= 2001 && XP <=5000 ) {
+    ELO = "Prata"
+}else if (XP >= 5001 && XP <=7000 ) {
+    ELO = "Ouro"
+}else if (XP >= 7001 && XP <=8000 ) {
+    ELO = "Platina"
+}else if (XP >= 8001 && XP <=9000 ) {
+    ELO = "Ascendente"
+}else if (XP >= 9001 && XP <=10000 ) {
+    ELO = "Imortal"
+}else if(XP >= 10001 ) {
+    ELO = "Radiante"
+}
+
+console.log(`O Herói de nome: ${NomeHeroi} está no nível de: ${ELO}`);
